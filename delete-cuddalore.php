137202de1343e500063e4b26fd22fc1cce7a59ba
shakdawoodh <muhamedrafiq123@gmail.com>
2022-01-01 09:41:47 +0530

    Adding new files.

--- delete-cuddalore.php
+++
@@ -1,6 +0,0 @@
-<?php 
- $conn=mysqli_connect("localhost","root","","sundaramani-travels");
- $del_query="delete from cuddalore where id=".$_GET['id'];
- $result=mysqli_query($conn,$del_query);
-header("Location:dashboard.php?culdel=succ")
-?>
--- delete-kasi.php
+++
@@ -1,6 +0,0 @@
-<?php 
- $conn=mysqli_connect("localhost","root","","sundaramani-travels");
- $del_query="delete from kasi where id=".$_GET['id'];
- $result=mysqli_query($conn,$del_query);
-header("Location:kasi.php?kasdel=succ")
-?>
